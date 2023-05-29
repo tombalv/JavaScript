03851aec29796a0a3b0b35d52087a4240e49dcb5
tomasbalvocius <tomasbalv@gmail.com>
2023-05-29 13:50:17 +0300

    array

--- js/05-array.js
+++ js/05-array.js
@@ -117,3 +117,28 @@ console.log('                 '.length );
 /* Apskaiciuoti verte is sarasu kuriu verciu tipas yra skaiciai, pagal pateikta logika.
 a. 1-2+3-4+5.
 b. Rezultata isvesti i console. */
+
+function getPlanetName(id){
+    var name;
+    switch(id){
+      case 1:
+        name = 'Mercury'
+      case 2:
+        name = 'Venus'
+      case 3:
+        name = 'Earth'
+      case 4:
+        name = 'Mars'
+      case 5:
+        name = 'Jupiter'
+      case 6:
+        name = 'Saturn'
+      case 7:
+        name = 'Uranus'
+      case 8:
+        name = 'Neptune'
+    }
+    
+    return name;
+  }
+console.log()
