bccf141add54510345593c61bbb262406708c12d
tomasbalvocius <tomasbalv@gmail.com>
2023-06-01 15:14:37 +0300

    update

--- js/04-function.js
+++ js/04-function.js
@@ -1,44 +1,79 @@
-console.clear();
-// Funkcija automatiskai prideda return: undefined.
-function sum(a, b) {
-    console.log('>>>', a, b);
-    return a + b;
-}
+// console.clear();
+// // Funkcija automatiskai prideda return: undefined.
+// function sum(a, b) {
+//   console.log(">>>", a, b);
+//   return a + b;
+// }
 
-const s1 = sum(2, 2);
-console.log(s1);
-const s2 = sum(7, 5);
-console.log(s2);
-const s3 = sum(-16, -15);
-console.log(s3);
-const s4 = sum(1);
-console.log(s4);
-const s5 = sum(3.14, 2.72);
-console.log(s5);
+// // Hi, my name is Nepetras
+// // Hi, my name is Nemaryte
+// // Hi, my name is Nejonas
+// // Hi, my name is Neona
+// function hi(name) {
+//   console.log(name);
+//   return `Hi, my name is ${name}`;
+// }
+// console.log(hi("Nepetras"));
+// console.log(hi("Nemaryte"));
+// console.log(hi("Nejonas"));
+// console.log(hi("Neona"));
 
-// Hi, my name is Nepetras
-// Hi, my name is Nemaryte
-// Hi, my name is Nejonas
-// Hi, my name is Neona
-function hi(name) {
-    console.log(name);
-    return `Hi, my name is ${name}`;
-}
-console.log(hi('Nepetras'));
-console.log(hi('Nemaryte'));
-console.log(hi('Nejonas'));
-console.log(hi('Neona'));
+// // Sveiki, as Nepetras ir man 16 metu.
+// // Sveiki, as Nemaryte ir man 160 metu.
+// // Sveiki, as Nejonas ir man 36 metu.
+// // Sveiki, as Neona ir man 99 metu.
+
+// function prisistatymas(name, age) {
+//   return `Sveiki, as ${name} ir man ${age} metu.`;
+// }
+// console.log(prisistatymas("Nepetras", 16));
+// console.log(prisistatymas("Nemaryte", 160));
+// console.log(prisistatymas("Nejonas", 36));
+// console.log(prisistatymas("Neona", 99));
+
+// const s1 = sum(2, 2);
+// console.log(s1);
+// const s2 = sum(7, 5);
+// console.log(s2);
+// const s3 = sum(-16, -15);
+// console.log(s3);
+// const s4 = sum(1, 5);
+// console.log(s4);
+// const s5 = sum(3.14, 2.72);
+// console.log(s5);
+
+// function sum(firstNumber, secondNumber) {
+//   return firstNumber + secondNumber;
+// }
+
+// const r1 = pirmaRaide("Jonas");
+// const r2 = pirmaRaide("Maryte");
+// const r3 = pirmaRaide("Adomas");
+
+// function pirmaRaide(vardas) {
+//   return vardas[0] + vardas[1];
+// }
+// console.log("Pirmos raides", r1, r2, r3);
+
+// function invicialai(vardas, pavarde) {
+//   return `${vardas[0]}.${pavarde[0]}`;
+// }
+
+// const ini1 = invicialai("Chuck", "Norris");
+// const ini2 = invicialai("Pamela", "Anderson");
+
+// console.log("Inicialiai 1:", ini1);
+// console.log("Inicialiai 2:", ini2);
 
 // Sveiki, as Nepetras ir man 16 metu.
 // Sveiki, as Nemaryte ir man 160 metu.
 // Sveiki, as Nejonas ir man 36 metu.
 // Sveiki, as Neona ir man 99 metu.
 
-function prisistatymas(name, age) {
-    return `Sveiki, as ${name} ir man ${age} metu.`;
+function pristatymas(name, age) {
+  return `Sveiki, as ${name} ir man ${age} metu.`;
 }
-console.log(prisistatymas('Nepetras', 16));
-console.log(prisistatymas('Nemaryte', 160));
-console.log(prisistatymas('Nejonas', 36));
-console.log(prisistatymas('Neona', 99));
-
+console.log(pristatymas("Nepetras", 16));
+console.log(pristatymas("Nemaryte", 160));
+console.log(pristatymas("Nejonas", 36));
+console.log(pristatymas("Neona", 99));
--- js/05-array.js
+++ js/05-array.js
@@ -1,144 +1,165 @@
-console.clear();
-/*
-Array - arejus, sarasas, listas, matrica, masyvas(slang)
+// console.clear();
+// /*
+// Array - arejus, sarasas, listas, matrica, masyvas(slang)
 
-Masyvas(array) prasideda index = 0;
-array.length - masyve esanciu reiksmiu kiekis;
-*/
+// Masyvas(array) prasideda index = 0;
+// array.length - masyve esanciu reiksmiu kiekis;
+// */
 
-const empty = [];
-console.log(empty);
+// const empty = [];
+// console.log(empty);
 
-// To paties tipo informacija skaiciai = skaiciai, raides = raides.
-// Eiliskumas      0  1  2  3  4
-const pazymiai = [10, 2, 8, 4, 6];
-console.log(pazymiai);
+// // To paties tipo informacija skaiciai = skaiciai, raides = raides.
+// // Eiliskumas      0  1  2  3  4
+// const pazymiai = [10, 2, 8, 4, 6];
+// console.log(pazymiai);
 
-// Random informacija nerekomenduojama naudoti.
-const random = [1, 'antras', 3, '4', 3.14, 'asdasd'];
+// // Random informacija nerekomenduojama naudoti.
+// const random = [1, "antras", 3, "4", 3.14, "asdasd"];
 
-// Eiliskumas       0           1           2         3
-const vardai = ['Nepetras', 'Nemaryte', 'Nejonas', 'Neona'];
-console.log(vardai);
+// // Eiliskumas       0           1           2         3
+// const vardai = ["Nepetras", "Nemaryte", "Nejonas", "Neona"];
+// console.log(vardai);
 
-const vardas1 = vardai[0];
-console.log(vardas1);
-const vardas2 = vardai[1];
-console.log(vardas2);
+// const vardas1 = vardai[0];
+// console.log(vardas1);
+// const vardas2 = vardai[1];
+// console.log(vardas2);
 
-console.log(vardai[2]);
-console.log(vardai[3]);
-// Naudojama tik paskutine reiksme
-console.log(vardai[0, 1, 2, 3]);
-console.log(vardai[3, 2, 1, 0]);
+// console.log(vardai[2]);
+// console.log(vardai[3]);
+// // Naudojama tik paskutine reiksme
+// console.log(vardai[(0, 1, 2, 3)]);
+// console.log(vardai[(3, 2, 1, 0)]);
 
-// Eiliskumas   0  1  2  3  4
-const marks = [10, 2, 8, 4, 6];
-console.log(marks);
-console.log(marks[0]);
-console.log(marks[1]);
-console.log(marks[2]);
-console.log(marks[3]);
-console.log(marks[4]);
+// // Eiliskumas   0  1  2  3  4
+// const marks = [10, 2, 8, 4, 6];
+// console.log(marks);
+// console.log(marks[0]);
+// console.log(marks[1]);
+// console.log(marks[2]);
+// console.log(marks[3]);
+// console.log(marks[4]);
 
-console.clear();
+// console.clear();
 
-const marks1 = [10, 9, 8];
-const marks2 = [6, 7, 8];
-const marks3 = [5, 5, 5];
+// const marks1 = [10, 9, 8];
+// const marks2 = [6, 7, 8];
+// const marks3 = [5, 5, 5];
 
-// Vidurkis 
-function average(marks) {
-    let sum = 0;
-    sum += marks[0];
-    sum += marks[1];
-    sum += marks[2];
-    return sum / marks.length;
+// // Vidurkis
+// function average(marks) {
+//   let sum = 0;
+//   sum += marks[0];
+//   sum += marks[1];
+//   sum += marks[2];
+//   return sum / marks.length;
+// }
+
+// console.log("Vidurkis 1:", marks1, "=", average(marks1));
+// console.log("Vidurkis 2:", marks2, "=", average(marks2));
+// console.log("Vidurkis 3:", marks3, "=", average(marks3));
+
+// const dict1 = ["pirmas", "zodis", "labas"];
+// const dict2 = ["Labas", "rytas", "Lietuva"];
+// const dict3 = ["Labas", "rytas", "suraitytas"];
+
+// // a, b, c.
+
+// function dictionary(wordList) {
+//   console.log(wordList);
+//   let result = "Zodynas: ";
+//   result += wordList[0] + ", ";
+//   result += wordList[1] + ", ";
+//   result += wordList[2] + ". ";
+//   return result;
+//   // Arba backtick return `Zodynas: ${wordList[0]}, ${wordList[1]}, ${wordList[2]}.`;
+// }
+// console.log(dictionary(dict1));
+// console.log(dictionary(dict2));
+// console.log(dictionary(dict3));
+// /* Arba nepavyks panaudoti kai bus daug zodziu ir jei kiekvienas zodynas sudarys kintama kieki zodziu.
+// Del to geriau naudoti logika kuri gauna vienu atveju viena zodyna kitokiu atveju kita.
+// console.log(`Zodynas: ${wordList[0]}, ${wordList[1]}, ${wordList[2]}.`);
+// console.log(`Zodynas: ${wordList[0]}, ${wordList[1]}, ${wordList[2]}.`);
+// console.log(`Zodynas: ${wordList[0]}, ${wordList[1]}, ${wordList[2]}.`); */
+
+// console.clear();
+
+// // Istraukti kubine sakni tai tas pats kas pakelti viena treciaja.
+// console.log(2 ** 3);
+// console.log(9 ** 2);
+// // Istraukti kubine sakni tai tas pats kas pakelti viena treciaja.
+// console.log(9 ** 0.5);
+// console.log(8 ** 0.33333333);
+
+// // Zodzio arba sakinio skaidymas po viena pozicija(simboli). String yra supaprastintas masyvas(array).
+// const text = "Labas rytas";
+// console.log(text);
+// console.log(text[0]);
+// console.log(text[1]);
+// console.log(text[2]);
+// console.log(text[3]);
+// console.log(text[4]);
+// console.log(text[5]);
+// console.log(text[6]);
+// console.log(text[7]);
+// console.log(text[8]);
+// console.log(text[9]);
+// console.log(text[10]);
+// // .length pasako kiek yra poziciju(simboliu) masyve(array);
+// console.log(text.length);
+// // Kartu skaiciuojasi ir tuscios eilutes. String ir Array ir giminingi bet ne panasus.
+// console.log("                 ".length);
+
+// /* Apskaiciuoti verte is sarasu kuriu verciu tipas yra skaiciai, pagal pateikta logika.
+// a. 1-2+3-4+5.
+// b. Rezultata isvesti i console. */
+
+// function getPlanetName(id) {
+//   var name;
+//   switch (id) {
+//     case 1:
+//       name = "Mercury";
+//       break;
+//     case 2:
+//       name = "Venus";
+//     case 3:
+//       name = "Earth";
+//     case 4:
+//       name = "Mars";
+//     case 5:
+//       name = "Jupiter";
+//     case 6:
+//       name = "Saturn";
+//     case 7:
+//       name = "Uranus";
+//     case 8:
+//       name = "Neptune";
+//   }
+
+//   return name;
+// }
+// console.log();
+
+const stud1 = "Jonas";
+const marks1 = [10, 2, 8, 2, 8, 2, 8];
+
+const stud2 = "Maryte";
+const marks2 = [10, 9, 8];
+
+const stud3 = "Petras";
+const marks3 = [5, 6, 7];
+
+const stud4 = "Ona";
+const marks4 = [5, 5, 5, 5, 5, 5, 5, 5];
+
+function studentIntro(studentName, marks) {
+  const lastMark = marks[marks.length - 1];
+  return studentName + ": paskutinis pazymys yra " + lastMark + ".";
 }
 
-console.log('Vidurkis 1:', marks1, '=', average(marks1));
-console.log('Vidurkis 2:', marks2, '=', average(marks2));
-console.log('Vidurkis 3:', marks3, '=', average(marks3));
-
-
-const dict1 = ['pirmas', 'zodis', 'labas'];
-const dict2 = ['Labas', 'rytas', 'Lietuva'];
-const dict3 = ['Labas', 'rytas', 'suraitytas'];
-
-// a, b, c.
-
-function dictionary(wordList) {
-    console.log(wordList);
-    let result = 'Zodynas: ';
-    result += wordList[0] + ', ';
-    result += wordList[1] + ', ';
-    result += wordList[2] + '. ';
-    return result;
-    // Arba backtick return `Zodynas: ${wordList[0]}, ${wordList[1]}, ${wordList[2]}.`;
-}
-console.log(dictionary(dict1));
-console.log(dictionary(dict2));
-console.log(dictionary(dict3));
-/* Arba nepavyks panaudoti kai bus daug zodziu ir jei kiekvienas zodynas sudarys kintama kieki zodziu.
-Del to geriau naudoti logika kuri gauna vienu atveju viena zodyna kitokiu atveju kita.
-console.log(`Zodynas: ${wordList[0]}, ${wordList[1]}, ${wordList[2]}.`);
-console.log(`Zodynas: ${wordList[0]}, ${wordList[1]}, ${wordList[2]}.`);
-console.log(`Zodynas: ${wordList[0]}, ${wordList[1]}, ${wordList[2]}.`); */
-
-console.clear();
-
-// Istraukti kubine sakni tai tas pats kas pakelti viena treciaja.
-console.log(2 ** 3);
-console.log(9 ** 2);
-// Istraukti kubine sakni tai tas pats kas pakelti viena treciaja.
-console.log(9 ** 0.5);
-console.log(8 ** 0.33333333);
-
-// Zodzio arba sakinio skaidymas po viena pozicija(simboli). String yra supaprastintas masyvas(array).
-const text = 'Labas rytas';
-console.log(text);
-console.log(text[0]);
-console.log(text[1]);
-console.log(text[2]);
-console.log(text[3]);
-console.log(text[4]);
-console.log(text[5]);
-console.log(text[6]);
-console.log(text[7]);
-console.log(text[8]);
-console.log(text[9]);
-console.log(text[10]);
-// .length pasako kiek yra poziciju(simboliu) masyve(array);
-console.log(text.length);
-// Kartu skaiciuojasi ir tuscios eilutes. String ir Array ir giminingi bet ne panasus.
-console.log('                 '.length );
-
-
-/* Apskaiciuoti verte is sarasu kuriu verciu tipas yra skaiciai, pagal pateikta logika.
-a. 1-2+3-4+5.
-b. Rezultata isvesti i console. */
-
-function getPlanetName(id){
-    var name;
-    switch(id){
-      case 1:
-        name = 'Mercury'
-      case 2:
-        name = 'Venus'
-      case 3:
-        name = 'Earth'
-      case 4:
-        name = 'Mars'
-      case 5:
-        name = 'Jupiter'
-      case 6:
-        name = 'Saturn'
-      case 7:
-        name = 'Uranus'
-      case 8:
-        name = 'Neptune'
-    }
-    
-    return name;
-  }
-console.log()
+console.log(studentIntro(stud1, marks1));
+console.log(studentIntro(stud2, marks2));
+console.log(studentIntro(stud3, marks3));
+console.log(studentIntro(stud4, marks4));
