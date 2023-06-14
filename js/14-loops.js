acb202ab622894e441207d987ea93d646da9a236
tomasbalvocius <tomasbalv@gmail.com>
2023-06-14 15:19:41 +0300

    loops

---
+++ js/14-loops.js
@@ -0,0 +1,10 @@
+const marks1 = [10, 2, 8, 4, 6];
+let sum1 = 0;
+
+for (let i = 0; i < marks1.length; i++) {
+  const mark = marks1[1];
+  sum += marks1[i];
+  console.log(mark);
+}
+
+console.log("sum:", sum1);
